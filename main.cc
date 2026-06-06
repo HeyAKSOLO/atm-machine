f30fe7ed71aced2c7ed5c2ac2523a6f475714476
HeyAK <heykmorph@gmail.com>
2026-06-06 17:27:35 +0530

    add constructor to each class and inherit account

--- main.cc
+++ main.cc
@@ -4,15 +4,25 @@
 #include <iomanip>
 using namespace std;
 class Account{
-    
     protected:
-    string username;
-    string pin;
-    double balance;
+        string username;
+        string pin;
+        double balance;
+
+    public:
+        Account(string acc_username, string acc_pin, double acc_balance) : username(acc_username), pin(acc_pin), balance(acc_balance) {}
     
 };
-class SavingAccount : public Account{};
-class CurrentAccount : public Account{};
+class SavingAccount : public Account{
+    public:
+        SavingAccount(string sacc_uname, string sacc_pin) : Account(sacc_uname, sacc_pin, 0.0){}
+        
+    };
+    
+class CurrentAccount : public Account{
+    public:
+        CurrentAccount(string cacc_uname, string cacc_pin) : Account(cacc_uname, cacc_pin, 0.0){}
+};
 class ATM{
     private:
     vector<SavingAccount> savingsAccounts;
@@ -20,5 +30,7 @@ class ATM{
     
 };
 int main(){
+    SavingAccount s1("Pona Tona" , "345434");
+
     return 0;
 }
