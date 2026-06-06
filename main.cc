cd70e57de15517bfb965087b8d681e46fd9480b4
HeyAK <heykmorph@gmail.com>
2026-06-06 16:51:48 +0530

    create ATM project structure

--- main.cc
+++ main.cc
@@ -3,7 +3,17 @@
 #include <vector>
 #include <iomanip>
 using namespace std;
+class Account{
+    private:
+        string username;
+        int pin;
+        double balance;
+        vector<string> accounts;
 
+};
+class SavingAccount : public Account{};
+class CurrentAccount : public Account{};
+class ATM{};
 int main(){
     return 0;
 }
